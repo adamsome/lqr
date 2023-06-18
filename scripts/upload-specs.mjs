d01874c5389e66703415b67cc0dc428a426c0fb8
adamsome <adamsome@gmail.com>
2023-06-18 01:44:46 -0400

    Update upload script

--- scripts/upload-specs.mjs
+++ scripts/upload-specs.mjs
@@ -1,3 +1,4 @@
+import { compareAsc, parseISO } from 'date-fns'
 import { readFileSync } from 'fs'
 import { MongoClient } from 'mongodb'
 import { resolve } from 'path'
@@ -10,7 +11,7 @@ const specsPath = resolve(process.cwd(), '../json/specs.json')
 const specsJson = readFileSync(specsPath, 'utf-8')
 const specDict = JSON.parse(specsJson)
 const specIDs = Object.keys(specDict)
-const specs = specIDs.map((id) => specDict[id])
+let specs = specIDs.map((id) => specDict[id])
 
 const { MONGODB_DB, MONGODB_URI } = process.env
 const client = new MongoClient(MONGODB_URI)
@@ -18,8 +19,37 @@ await client.connect()
 const lqrDB = client.db(MONGODB_DB)
 const specColl = lqrDB.collection('spec')
 
+const prevSpecs = await specColl
+  .find({}, { projection: { _id: false } })
+  .toArray()
+const prevSpecDict = prevSpecs.reduce((acc, s) => {
+  acc[s.id] = s
+  return acc
+}, {})
+specs = specs.map((curr) => {
+  const prev = prevSpecDict[curr.id] ?? {}
+  const ingredients = curr.ingredients.map((it, i) => ({
+    ...(prev.ingredients?.[i] ?? {}),
+    ...it,
+  }))
+  let createdAt = curr.createdAt
+  if (prev.createdAt) {
+    if (compareAsc(parseISO(prev.createdAt), parseISO(createdAt)) === -1) {
+      createdAt = prev.createAt
+    }
+  }
+  let updatedAt = prev.updatedAt ?? curr.updatedAt
+  return {
+    ...prev,
+    ...curr,
+    ingredients,
+    createdAt,
+    updatedAt,
+  }
+})
+
 await specColl.deleteMany({})
 const res = await specColl.insertMany(specs, { upsert: true })
-console.log('res', res)
+console.log('res', (res?.insertedCount ?? 0) > 0 ? res.insertedCount : res)
 
 process.exit()
