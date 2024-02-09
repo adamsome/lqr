d631b7fd749f6ffd8d0bdc7355e16614480480f7
adamsome <adamsome@gmail.com>
2024-02-09 00:39:01 -0500

    Add users to follow to following page

--- app/u/[username]/following/list.tsx
+++ app/u/[username]/following/list.tsx
@@ -1,3 +1,4 @@
+import { add, isPast, parseISO } from 'date-fns'
 import invariant from 'tiny-invariant'
 
 import { Empty } from '@/app/components/empty'
@@ -5,7 +6,12 @@ import { Stack } from '@/app/components/layout/stack'
 import { FollowButtonContainer } from '@/app/components/user/follow-button-container'
 import { UserAvatarRow } from '@/app/components/user/user-avatar-row'
 import { getAllFollowing } from '@/app/lib/model/follow'
-import { getAllUsers, getCurrentUser } from '@/app/lib/model/user'
+import {
+  getAllUsers,
+  getCurrentUser,
+  getMostRecentActedUsers,
+} from '@/app/lib/model/user'
+import type { User } from '@/app/lib/types'
 import { cn, toDict } from '@/app/lib/utils'
 import { EnsureFollows } from '@/app/u/[username]/following/ensure-follows'
 
@@ -15,7 +21,7 @@ type Props = {
 }
 
 export async function List({ className, username }: Props) {
-  const { user, currentUser } = await getCurrentUser(username)
+  const { user, currentUser, isCurrentUser } = await getCurrentUser(username)
 
   invariant(user, `User not found.`)
 
@@ -26,16 +32,48 @@ export async function List({ className, username }: Props) {
   const currentUserFollowing = await getAllFollowing(currentUser?.id)
   const byFollowee = toDict(currentUserFollowing, ({ followee }) => followee)
 
+  let usersToFollow: User[] = []
+  if (currentUser && isCurrentUser) {
+    const excluded = userFollowing
+      .filter(
+        ({ follows, followedAt }) =>
+          follows || !isPast(add(parseISO(followedAt), { hours: 1 })),
+      )
+      .map(({ followee }) => followee)
+    usersToFollow = await getMostRecentActedUsers({
+      exclude: [currentUser.id, ...excluded],
+      limit: 15,
+    })
+  }
+
   return (
-    <Stack className={cn('w-full', className)} gap={4}>
-      {followingUsers.map((u) => (
-        <EnsureFollows key={u.id} follow={byFollowee[u.id]}>
-          <UserAvatarRow user={u}>
-            <FollowButtonContainer username={u.username} />
-          </UserAvatarRow>
-        </EnsureFollows>
-      ))}
-      {followingUsers.length === 0 && (
+    <Stack className={cn(className)} gap={7}>
+      {followingUsers.length > 0 && (
+        <Stack className={'w-full'} gap={4}>
+          {followingUsers.map((u) => (
+            <EnsureFollows key={u.id} follow={byFollowee[u.id]}>
+              <UserAvatarRow user={u}>
+                <FollowButtonContainer username={u.username} />
+              </UserAvatarRow>
+            </EnsureFollows>
+          ))}
+        </Stack>
+      )}
+      {usersToFollow.length > 0 && (
+        <Stack gap={3}>
+          <div className="text-xl text-muted-foreground font-medium tracking-wide [font-stretch:condensed]">
+            Users to Follow
+          </div>
+          <Stack gap={4}>
+            {usersToFollow.map((u) => (
+              <UserAvatarRow key={u.id} user={u}>
+                <FollowButtonContainer username={u.username} />
+              </UserAvatarRow>
+            ))}
+          </Stack>
+        </Stack>
+      )}
+      {followingUsers.length === 0 && usersToFollow.length === 0 && (
         <Empty title="Not following any other bar managers">
           Follow some more managers to have
           <br />
